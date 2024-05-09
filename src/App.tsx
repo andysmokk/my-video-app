032e937d6336007240f46d19cf313830e6c43842
Andrii Konovalov <andy.smokk@gmail.com>
2024-05-09 21:55:34 +0200

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return <h1 className="text-3xl underline font-bold">My Music App</h1>;
+  return <h2 className="text-3xl underline font-bold">My Music App</h2>;
 }
