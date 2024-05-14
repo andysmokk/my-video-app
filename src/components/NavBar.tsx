92609c71215979dda605a8ace7fd6dbd92641d4e
Andrii Konovalov <andy.smokk@gmail.com>
2024-05-14 21:38:24 +0200

    added form to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,48 @@
 import { Link } from "react-router-dom";
 
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 import { BsYoutube } from "react-icons/bs";
 
 const NavBar = () => {
   return (
     <div
-      className="flex gap-10 items-center px-14 h-14 bg-[#212121] 
-    opacity-95 sticky top-0 z-50"
+      className="flex justify-between items-center px-14 h-14 bg-[#212121]
+     opacity-95 sticky top-0 z-50"
     >
       <div className="flex gap-8 item-center text-2xl">
-        <GiHamburgerMenu />
-      </div>
-      <Link to="/">
-        <div className="flex gap-1 items-center justify-center">
-          <BsYoutube className="text-3xl text-red-600" />
-          <span className="text-xl font-medium">My Video App</span>
+        <div>
+          <GiHamburgerMenu />
         </div>
-      </Link>
+        <Link to="/">
+          <div className="flex gap-1 items-center justify-center">
+            <BsYoutube className="text-3xl text-red-600" />
+            <span className="text-xl font-medium">My Video App</span>
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center gap-5">
+        <form action="">
+          <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
+            <div className="flex gap-4 items-center pr-5">
+              <div>
+                <AiOutlineSearch className="text-xl" />
+              </div>
+              <input
+                type="text"
+                className="w-96 bg-zinc-900 focus:outline-none border-none"
+              />
+              <AiOutlineClose className="cursor-pointer text-xl" />
+            </div>
+            <button
+              className="h-10 w-16 flex items-center justify-center
+               bg-zinc-800"
+            >
+              <AiOutlineSearch className="text-xl" />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
